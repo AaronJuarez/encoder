00b7857a1d0e64d1ee5e2fd00c3df72526388658
encomp <edgar.martinez.rico@gmail.com>
2016-06-26 15:16:39 -0700

    Add additional test cases

--- src/test/java/com/leantass/encoder/ParamEncoderImplTest.java
+++ src/test/java/com/leantass/encoder/ParamEncoderImplTest.java
@@ -1,5 +1,8 @@
 package com.leantass.encoder;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -11,8 +14,26 @@ import org.mockito.runners.MockitoJUnitRunner;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class ParamEncoderImplTest {
-  private ParamEncoderImpl instance;
 
-  @Mock ParamEncoderObject paramEncoderObject;
-  @Mock ParamEncoderArray paramEncoderArray;
+  @Mock
+  ParamEncoderObject paramEncoderObject;
+  @Mock
+  ParamEncoderArray paramEncoderArray;
+  private ParamEncoderImpl instance;
+  @Rule
+  public final ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void shouldThrowNullPointerExceptionMissingParamEncoderObject() {
+    thrown.expect(NullPointerException.class);
+    thrown.expectMessage("ParamEncoderObject is missing.");
+    new ParamEncoderImpl(null, paramEncoderArray);
+  }
+
+  @Test
+  public void shouldThrowNullPointerExceptionMissingParamEncoderArray() {
+    thrown.expect(NullPointerException.class);
+    thrown.expectMessage("ParamEncoderArray is missing.");
+    new ParamEncoderImpl(paramEncoderObject, null);
+  }
 }
