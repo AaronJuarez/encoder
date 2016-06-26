c3410ee78bc90ecf53e28de39da773a72124c88b
encomp <edgar.martinez.rico@gmail.com>
2016-06-26 14:20:05 -0700

    Add additional Java doc

--- src/main/java/com/leantass/encoder/ParamEncoderImpl.java
+++ src/main/java/com/leantass/encoder/ParamEncoderImpl.java
@@ -9,7 +9,53 @@ import java.util.Map.Entry;
 import java.util.SortedMap;
 
 /**
- * Specifies the behavior to encode parameters.
+ * Specifies the behavior to encode a {@link SortedMap} of parameters. The parameters could be
+ * specified as {@link Integer} or {@link String}. For example, consider a field <b>x</b> with
+ * maxWidth of 2 using this encoding:
+ *
+ * <ol>
+ *   <li> Integer ({@link TruncationStyle#INTEGER}):
+ *     <ul>
+ *       <li>A value of 1 will be encoded as <b>x=1</b></li>
+ *       <li>A value of 12 will be encoded as <b>x=12</b></li>
+ *       <li>A value of 123 will be encoded as <b>x=99</b>, since 99 is the largest positive
+ *       number that can be encoded with 2 characters</li>
+ *       <li>A value of -1 will be encoded as <b>x=-1</b></li>
+ *       <li>A value of -12 will be encoded as <b>x=-9</b>, since -9 is the smallest negative number
+ *       that can be encoded with 2 characters.</li>
+ *     </ul>
+ *   </li>
+ *   <li>
+ *     String <b>left truncation</b> ({@link TruncationStyle#STRING_LEFT}):
+ *     <ul>
+ *       <li>A value of A will be encoded as <b>x=A</b></li>
+ *       <li>A value of AB will be encoded as <b>x=AB</b></li>
+ *       <li>A value of ABC will be encoded as <b>x=BC</b>, truncating on the left</li>
+ *       <li>A value of ABCD will be encoded as <b>x=CD</b>, truncating on the left</li>
+ *     </ul>
+ *   </li>
+ *   <li>
+ *     String <b>right truncation</b> ({@link TruncationStyle#STRING_RIGHT}):
+ *     <ul>
+ *       <li>A value of A will be encoded as <b>x=A</b></li>
+ *       <li>A value of AB will be encoded as <b>x=AB</b></li>
+ *       <li>A value of ABC will be encoded as <b>x=AB</b>, truncating on the right</li>
+ *       <li>A value of ABCD will be encoded as <b>x=AB</b>, truncating on the right</li>
+ *     </ul>
+ *   </li>
+ * </ol>
+ *
+ * A parameter can be also be specified as {@code String[]} and it will be encoded as follow. For
+ * example, consider an array field <b>x</b> with maxArrayWidth 10 using
+ * {@link ParamEncoder.TruncationStyle#STRING_RIGHT} truncation style and maxElemWidth of 3:
+ * <ul>
+ *   <li>A value of ”AB”, ”CD” will be encoded as <b>x=[AB,CD]</b></li>
+ *   <li>A value of ”AB”, ”CDEF” will be encoded as <b>x=[AB,CDE]</b>; the second element got
+ *   truncated in STRING_RIGHT style.</li>
+ *   <li>A value of ”AB”, ”CDEF”, ”GHIJ”, ”K” will be encoded as <b>x=[AB,CDE]</b>; including the
+ *   encoding of third element will make the encoded string longer than 10 characters, so it and
+ *   all subsequent elements are dropped from the output</li>
+ * </ul>
  *
  * @author jovanimtzrico@gmail.com (Jovani Rico)
  */
@@ -21,8 +67,16 @@ public class ParamEncoderImpl implements ParamEncoder {
   private final ParamEncoderObject paramEncoderObject;
   private final ParamEncoderArray paramEncoderArray;
 
+  /**
+   * Create a new instance of {@code ParamEncoderImpl}.
+   *
+   * @param paramEncoderObject specifies the encoder that will be used to encode {@link Integer}
+   *                           and {@link String}
+   * @param paramEncoderArray specifies the encoded that will be used to encode {@code String[]}
+   */
   @Inject
-  public ParamEncoderImpl(ParamEncoderObject paramEncoderObject, ParamEncoderArray paramEncoderArray) {
+  public ParamEncoderImpl(ParamEncoderObject paramEncoderObject,
+                          ParamEncoderArray paramEncoderArray) {
     this.paramEncoderObject = checkNotNull(paramEncoderObject, "ParamEncoderObject is missing.");
     this.paramEncoderArray = checkNotNull(paramEncoderArray, "ParamEncoderArray is missing.");
   }
