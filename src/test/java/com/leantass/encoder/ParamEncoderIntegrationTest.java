df08d67e9cd8cadffa85c8debb89e1e26aa2a3cf
encomp <edgar.martinez.rico@gmail.com>
2016-06-26 14:46:07 -0700

    Add additional test cases

--- src/test/java/com/leantass/encoder/ParamEncoderIntegrationTest.java
+++ src/test/java/com/leantass/encoder/ParamEncoderIntegrationTest.java
@@ -2,8 +2,10 @@ package com.leantass.encoder;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.SortedMap;
 import java.util.TreeMap;
 
+import com.google.common.collect.ImmutableSortedMap;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -28,81 +30,74 @@ public class ParamEncoderIntegrationTest {
   }
 
   @Test
-  public void addInteger() {
+  public void shouldEncodeIntegers() {
     encoder.addFieldTruncationRule("int2", ParamEncoder.TruncationStyle.INTEGER, 3);
     encoder.addFieldTruncationRule("int3", ParamEncoder.TruncationStyle.INTEGER, 3);
     encoder.addFieldTruncationRule("int3", ParamEncoder.TruncationStyle.INTEGER, 2);
+    SortedMap<String, Object> immutableSortedMap =
+        ImmutableSortedMap.of(
+            "int1", (Object) 10,
+            "int2", (Object) 100,
+            "int3", (Object) 100,
+            "int4", (Object) 10);
+    String result = encoder.encode(immutableSortedMap);
 
-    String result = encoder.encode(new TreeMap<String, Object>() {
-      {
-        put("int1", 10);
-        put("int2", 100);
-        put("int3", 100);
-        put("int4", 10);
-      }
-    });
     assertEquals(result, "int2=100&int3=99");
   }
 
   @Test
-  public void testString() {
+  public void shouldEncodeStrings() {
     encoder.addFieldTruncationRule("left2", ParamEncoder.TruncationStyle.STRING_RIGHT, 3);
     encoder.addFieldTruncationRule("right3", ParamEncoder.TruncationStyle.STRING_LEFT, 2);
+    SortedMap<String, Object> immutableSortedMap =
+        ImmutableSortedMap.of(
+            "left1", (Object) "ABC",
+            "left2", (Object) "ABCE",
+            "right3", (Object) "ABCEF",
+            "right1", (Object) "ABC");
+    String result = encoder.encode(immutableSortedMap);
 
-    String result = encoder.encode(new TreeMap<String, Object>() {
-      {
-        put("left1", "ABC");
-        put("left2", "ABCE");
-        put("right3", "ABCEF");
-        put("right1", "ABC");
-      }
-    });
     assertEquals(result, "left2=ABC&right3=EF");
   }
 
   @Test
-  public void testArray() {
+  public void shouldEncodeArrays() {
     encoder.addFieldTruncationRule("int1", ParamEncoder.TruncationStyle.INTEGER, 3);
     encoder.addFieldTruncationRule("left2", ParamEncoder.TruncationStyle.STRING_LEFT, 4);
     encoder.addArrayTruncationRule("array1", 10, ParamEncoder.TruncationStyle.STRING_RIGHT, 3);
     encoder.addArrayTruncationRule("array3", 13, ParamEncoder.TruncationStyle.STRING_LEFT, 3);
     encoder.addArrayTruncationRule("array4", 10, ParamEncoder.TruncationStyle.STRING_RIGHT, 2);
+    SortedMap<String, Object> immutableSortedMap =
+        ImmutableSortedMap.of(
+            "array1", (Object) new String[]{"ABC", "EF", "IJ"},
+            "array2", (Object) new String[]{"ABC", "EF", "IJ"},
+            "array3", (Object) new String[]{"ABC", "DEFG", "HIJKL"},
+            "array4", (Object) new String[]{"ABC", "EF", "IJ"});
+    String result = encoder.encode(immutableSortedMap);
 
-    String result = encoder.encode(new TreeMap<String, Object>() {
-      {
-        put("array1", new String[]{"ABC", "EF", "IJ"});
-        put("array2", new String[]{"ABC", "EF", "IJ"});
-        put("array3", new String[]{"ABC", "DEFG", "HIJKL"});
-        put("array4", new String[]{"ABC", "EF", "IJ"});
-      }
-    });
     assertEquals(result, "array1=[ABC,EF]&array3=[ABC,EFG,JKL]&array4=[AB,EF,IJ]");
   }
 
   @Test
-  public void testMixobjects() {
+  public void shouldEncodeIntegerStringsAndArrays() {
     encoder.addFieldTruncationRule("int1", ParamEncoder.TruncationStyle.INTEGER, 3);
     encoder.addFieldTruncationRule("left2", ParamEncoder.TruncationStyle.STRING_LEFT, 4);
     encoder.addFieldTruncationRule("left3", ParamEncoder.TruncationStyle.STRING_RIGHT, 3);
     encoder.addArrayTruncationRule("array1", 10, ParamEncoder.TruncationStyle.STRING_RIGHT, 3);
+    SortedMap<String, Object> immutableSortedMap =
+        ImmutableSortedMap.of(
+            "array1", (Object) new String[]{"ABC", "EF", "IJ"},
+            "array2", (Object) new String[]{"ABC", "EF", "IJ"},
+            "left3", (Object) "ABC",
+            "left2", (Object) "ABC",
+            "int1", (Object) 100);
+    String result = encoder.encode(immutableSortedMap);
 
-    String result = encoder.encode(new TreeMap<String, Object>() {
-      {
-        put("array1", new String[]{"ABC", "EF", "IJ"});
-        put("array2", new String[]{"ABC", "EF", "IJ"});
-        put("array3", new String[]{"ABC", "EF", "IJ"});
-        put("array4", new String[]{"ABC", "EF", "IJ"});
-        put("int1", 100);
-        put("int2", 100);
-        put("left2", "ABC");
-        put("left3", "ABC");
-      }
-    });
     assertEquals(result, "array1=[ABC,EF]&int1=100&left2=ABC&left3=ABC");
   }
 
   @Test
-  public void testExampleObjects() {
+  public void shouldEncodeMixedParameters() {
     encoder.addFieldTruncationRule("left2", ParamEncoder.TruncationStyle.STRING_LEFT, 2);
     encoder.addFieldTruncationRule("left3", ParamEncoder.TruncationStyle.STRING_LEFT, 3);
     encoder.addFieldTruncationRule("right2", ParamEncoder.TruncationStyle.STRING_RIGHT, 2);
@@ -122,35 +117,35 @@ public class ParamEncoderIntegrationTest {
         put("ignored", "1234");
       }
     });
+
     assertEquals(result, "array1=[ABC,EF]&int2=99&int3=100&left2=BC&left3=ABC&right2=AB&right3=ABC");
   }
 
   @Test
-  public void testCharacterArray() {
-    thrown.expect(IllegalArgumentException.class);
-    encoder.addArrayTruncationRule("array1", 10, ParamEncoder.TruncationStyle.STRING_RIGHT, 3);
-    encoder.addArrayTruncationRule("array2", 13, ParamEncoder.TruncationStyle.STRING_LEFT, 3);
+  public void shouldThrowIllegalArgumentExceptionCannotEncodeObjects() {
+    encoder.addFieldTruncationRule("param1", ParamEncoder.TruncationStyle.STRING_LEFT, 3);
+    encoder.addFieldTruncationRule("param2", ParamEncoder.TruncationStyle.STRING_RIGHT, 3);
+    SortedMap<String, Object> immutableSortedMap =
+        ImmutableSortedMap.of(
+            "array1", (Object) "ABCDE",
+            "array2", (Object) new Object());
 
-    String result = encoder.encode(new TreeMap<String, Object>() {
-      {
-        put("array1", new Character[]{'A', 'B', 'C'});
-        put("array2", new Character[]{'D', 'E', 'F'});
-      }
-    });
-    assertEquals(result, "array1=[A,B,C]&array2=[D,E,F]");
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage("Encoding is not supported.");
+    encoder.encode(immutableSortedMap);
   }
 
   @Test
-  public void testObjectArray() {
-    thrown.expect(IllegalArgumentException.class);
+  public void shouldThrowIllegalArgumentExceptionCannotEncodeArrayOfCharacters() {
     encoder.addArrayTruncationRule("array1", 10, ParamEncoder.TruncationStyle.STRING_RIGHT, 3);
     encoder.addArrayTruncationRule("array2", 13, ParamEncoder.TruncationStyle.STRING_LEFT, 3);
+    SortedMap<String, Object> immutableSortedMap =
+        ImmutableSortedMap.of(
+            "array1", (Object) new Character[]{'A', 'B', 'C'},
+            "array2", (Object) new Character[]{'D', 'E', 'F'});
 
-    String result = encoder.encode(new TreeMap<String, Object>() {
-      {
-        put("array1", new Object[]{new Object(), new Object()});
-      }
-    });
-    System.out.println(result);
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage("Encoding is not supported.");
+    encoder.encode(immutableSortedMap);
   }
 }
